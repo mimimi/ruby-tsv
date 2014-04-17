09850a01fc16f0b9d637ca9b18c0fc5ddebf8f2f
Alex Rozumey <brain-geek@yandex.ua>
2014-04-17 16:16:52 +0300

    Add first working examples

--- lib/tsv.rb
+++ lib/tsv.rb
@@ -3,10 +3,18 @@ require "tsv/version"
 module TSV
   extend self
 
-  def parse(filepath)
+  def parse(filepath, opts = {})
     raise FileNameInvalidException if filepath.nil?
 
-    open(filepath, 'r')
+    res = []
+
+    open(filepath, 'r') do |f|
+      f.each_line do |line|
+        res.push line.chomp.split("\t")
+      end
+    end
+
+    res
   end
 
   class FileNameInvalidException < IOError
--- spec/lib/tsv_spec.rb
+++ spec/lib/tsv_spec.rb
@@ -1,22 +1,80 @@
 require File.join(File.dirname(__FILE__), '..', 'spec_helper.rb')
-
+puts File.join(File.dirname(__FILE__), '..', 'fixtures', 'example.tsv')
 describe TSV do
   describe "#parse" do
-    subject { lambda { TSV.parse(filename) } }
+    let(:header) { nil }
+    let(:tsv_path) { File.join(File.dirname(__FILE__), '..', 'fixtures', filename) }
 
-    context "when file is not found" do
-      let(:filename) { "AManThatWasntThere.tsv" }
+    subject { TSV.parse(tsv_path, header: header) }
 
-      it "returns FileNotFoundException" do
-        expect(subject).to raise_error(Errno::ENOENT)
+    describe "edge cases" do
+      subject { lambda { TSV.parse(tsv_path) } }
+
+      context "when file is not found" do
+        let(:tsv_path) { "AManThatWasntThere.tsv" }
+
+        it "returns FileNotFoundException" do
+          expect(subject).to raise_error(Errno::ENOENT)
+        end
+      end
+
+      context "when filename is invalid" do
+        let(:tsv_path) { nil }
+
+        it "returns FileNameInvalidException" do
+          expect(subject).to raise_error(TSV::FileNameInvalidException)
+        end
       end
     end
 
-    context "when filename is invalid" do
-      let(:filename) { nil }
+    context "when file is empty" do
+      let(:filename) { 'empty.tsv' }
 
-      it "returns FileNameInvalidException" do
-        expect(subject).to raise_error(TSV::FileNameInvalidException)
+      context "when requested without header" do
+        let(:header) { true }
+
+        it "returns empty array" do
+          expect(subject).to be_empty
+        end
+      end
+
+      context "when requested with header" do
+        let(:header) { false }
+
+        it "returns empty array" do
+          expect(subject).to be_empty
+        end
+      end
+    end
+
+    context "when file is valid" do
+      let(:filename) { 'example.tsv' }
+
+      context "when no block is passed" do
+        context "when requested without header" do
+          let(:header) { false }
+
+          it "returns its content as array of arrays" do
+            expect(subject).to eq [ ['first', 'second', 'third'],
+                                    ['0', '1', '2'],
+                                    ['one', 'two', 'three'],
+                                    ['weird data', 's@mthin#', 'else']]
+          end
+        end
+
+        context "when requested without header" do
+          let(:header) { false }
+
+          it "returns its content as array of hashes" do
+            expect(subject).to eq [ ['first', 'second', 'third'],
+                                    ['0', '1', '2'],
+                                    ['one', 'two', 'three'],
+                                    ['weird data', 's@mthin#', 'else']]
+          end
+        end
+      end
+      
+      context "when block is passed" do
       end
     end
   end
