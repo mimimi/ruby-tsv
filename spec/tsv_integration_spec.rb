a7ccfcb56b64de97d3c07e76f93eccc8fceb228d
Alex Rozumey <brain-geek@yandex.ua>
2014-06-04 17:24:30 +0300

    Finished integration spec.

--- spec/tsv_integration_spec.rb
+++ spec/tsv_integration_spec.rb
@@ -50,8 +50,46 @@ describe TSV do
   end
 
   describe "reading from string" do
-    context "when string is empty"
-    context "when string is invalid"
-    context "when string is valid"
+    subject { TSV.parse(IO.read(tsv_path), parameters).to_a }
+
+    context "when string is empty"  do
+      let(:filename) { 'empty.tsv' }
+
+      context "when requested without header" do
+        let(:header) { true }
+
+        it { expect(subject).to be_empty }
+      end
+
+      context "when requested with header" do
+        let(:header) { false }
+
+        it { expect(subject).to be_empty }
+      end
+    end
+
+    context "when string is invalid" do
+      subject { lambda { TSV.parse(IO.read(tsv_path), parameters).to_a } }
+      let(:filename) { 'broken.tsv' }
+
+      it "when file is broken" do
+        expect(subject).to raise_error TSV::Row::InputError
+      end
+    end
+
+    context "when string is valid" do
+      let(:filename) { 'example.tsv' }
+
+      context "when no block is passed" do
+        let(:parameters) { Hash.new }
+
+        it "returns its content as array of hashes" do
+          headers = %w{first second third}
+          expect(subject).to eq [ TSV::Row.new( ['0', '1', '2'], headers ),
+                                  TSV::Row.new( ['one', 'two', 'three'], headers ),
+                                  TSV::Row.new( ['weird data', 's@mthin#', 'else'], headers ) ]
+        end
+      end
+    end
   end
 end
